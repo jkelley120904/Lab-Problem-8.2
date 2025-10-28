bf066cda990db2f1e159be93a97c6b26fe83f215
Jenna Kelley <babygirl120904@icloud.com>
2025-10-27 23:28:55 -0400

    Completed countWords function

---
+++ P8-2/P8-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countWords(string str);
+
+int main(void)
+{
+	string str;
+
+	getline(cin, str);
+	cout << "Words " << countWords(str);
+
+	/*while (true)
+	{
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, str);
+		if (str == "Q" || str == "q") break;
+		cout << "Word count: " << countWords(str) << endl;
+	}*/
+}
+
+int countWords(string str)
+{
+	int numWords = 1;
+	char c;
+	for (int i = 0; i < str.length(); i++)
+	{
+		c = str[i];
+
+		if (c == ' ')
+		{
+			numWords++;
+		}
+
+	}
+	return numWords;
+}
